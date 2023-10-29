d18d9e526b15c95cedb10838989f20fcdd7fe6a9
Ivan122727 <ermolovivan2018@gmail.com>
2023-10-29 21:14:38 +0500

    schema

--- my_research/api/v1/schemas/research.py
+++ my_research/api/v1/schemas/research.py
@@ -1,10 +1,11 @@
+from typing import Optional
 from my_research.api.v1.schemas.base import BaseOutDBMSchema, BaseSchemaIn
 
 
 class ResearchOut(BaseOutDBMSchema):
     patient_id: int
-    type: str
-    filename: str
+    type: Optional[str]
+    filename: Optional[str]
     user_id: int
 
 
